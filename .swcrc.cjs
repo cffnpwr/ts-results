baf6b87b33ab6abef8ee94d46a73ebeb15c6d38b
cffnpwr <cffnpwr@gmail.com>
2023-10-15 22:13:50 +0900

    fix: unable use in esm

---
+++ .swcrc.cjs
@@ -0,0 +1,28 @@
+{
+  "$schema": "https://json.schemastore.org/swcrc",
+  "jsc": {
+    "parser": {
+      "syntax": "typescript",
+      "tsx": false,
+      "dynamicImport": false,
+      "decorators": true
+    },
+    "transform": {
+      "decoratorMetadata": true
+    },
+    "target": "es5",
+    "loose": false,
+    "externalHelpers": true,
+    "keepClassNames": true,
+    "baseUrl": "./",
+    "paths": {
+      "@/*": [
+        "./src/*"
+      ]
+    }
+  },
+  "minify": false,
+  "module": {
+    "type": "commonjs"
+  }
+}
